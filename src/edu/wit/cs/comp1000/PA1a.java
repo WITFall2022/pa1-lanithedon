cedcdec6d8ee5da400a776b256bfe1e11b83196d
401241 <401241@664-161476-LAB.schools.mybps.org>
2022-12-14 11:48:17 -0500

    This is Alani Rose's PA1 a & b

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,32 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		
+		    int yards = 0;
+			int feet = 0;
+			int inches = 0;
+			int total = 0;
+			Scanner input = new Scanner(System.in);
+			System.out.print (" Enter the number of yards in whole numbers") ;
+			yards = input.nextInt();
+			System.out.print (" Enter the number of feet in whole numbers") ;
+			feet = input.nextInt();
+			System.out.print (" Enter the number of inches in whole numbers");
+			inches = input.nextInt(); 
+			total = (yards*36) + (feet*12) + (inches) ;
+			
+			System.out.print (" the total number of inches : ");
+			 
+		}
+
 	}
 
-}
+
+
--- src/edu/wit/cs/comp1000/PA1b.java
+++ src/edu/wit/cs/comp1000/PA1b.java
@@ -1,11 +1,22 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import  java.util.Scanner;
+
 public class PA1b {
 
-	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+Scanner input = new Scanner(System.in);
+	
+ int yards = 0 ;
+ int feet = 0 ; 		 
+ int inches = 0 ;
+
+	System.out.println("enter number of inches in a whole number");
+	inches = input.nextInt();
+
+	int total = (yards * 3*12) + (feet*12) + inches;
+	System.out.println(total);
+
 	}
 
 }
